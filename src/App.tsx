b452ceed5e6c45c4b8c11f9761f5c09d705212f9
Manhhahoa <manhhahoa98@gmail.com>
2023-01-05 09:55:40 +0700

    check router

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,25 @@ import './App.css';
 import Header from './header/Header';
 import BannerSwiper from './page/banner_swiper/BannerSwiper';
 import Selectionbar from './selection_bar/Selectionbar';
-
+import { Routes, Route, BrowserRouter, Link } from 'react-router-dom'
 function App() {
+
   return (
-    <div className="App">
-      <Header></Header>
-      <Selectionbar></Selectionbar>
-      <BannerSwiper />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<div>
+          <Link to={'/header'} > <button>Next</button></Link>
+        </div>} path='/'>
+        </Route>
+        <Route element={<div>HOME</div>} path='/home'>
+          <Route element={<p>ok</p>} path='/home/ok'></Route>
+        </Route>
+        <Route element={<Header />} path='/header'></Route>
+        <Route element={<BannerSwiper />} path='/banner'></Route>
+        <Route path='*' element={<div>404</div>}></Route>
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
